42cd790e06dee600a92be298bbf6db6f21bd6b98
alperen-erol <alpereneroltr@gmail.com>
2023-07-07 03:52:58 +0300

    ghj

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,15 @@
+import {Link, link, NavLink} from "react-router-dom"
 const Nav = () => {
   return (
     <ul>
       <li>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <a href="/instructors">Instructors</a>
+        <Link to="/instructors">Instructors</Link>
       </li>
       <li>
-        <a href="/contact">Contact</a>
+        <Link to="/contact">Contact</Link>
       </li>
     </ul>
   );
